6352830b88004d0f0cdb470a91d875473dabc512
TheOfficialDevJosh <54554264+TheOfficialDevJosh@users.noreply.github.com>
2020-09-08 12:56:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 // const cors = require('cors')
 // app.use(cors())
-const server = require('http').Server(app)
+const server = require('https').Server(app)
 const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
